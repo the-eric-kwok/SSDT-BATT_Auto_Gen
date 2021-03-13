94ea3e9fa283aa19ca81d16b93cf4111c87614e1
Eric Kwok <erickwok404@gmail.com>
2021-03-13 13:10:23 +0800

    成功将文件解析为代码树

--- get_content_rewrite.py
+++ get_content_rewrite.py
@@ -34,6 +34,7 @@ def to_code_blocks(dsdt_content):
     }
     abandon_list = keyword_list - approve_list
     stack = []
+    blocks = []
     for i in rang:
         if dsdt_content[i] == '{':
             j = i - 1
@@ -43,21 +44,57 @@ def to_code_blocks(dsdt_content):
                 for keyword in keyword_list:
                     if keyword in rewind:
                         # append(offset, code_block_type)
-                        name = re.match("%s\s\((.*[,\)])" % keyword)
-                        stack.append((j, keyword, name))
+                        inside_bracket = re.match(
+                            "%s\s?\((.*)\)" % keyword, rewind)[1]
+                        stack.append((j, keyword, inside_bracket))
                         break
+                else:
                     j -= 1
                     if j < 0:
                         raise RuntimeError(
                             "Offset less than zero. Please re-check keyword_list!")
+                    continue
+                break
 
         elif dsdt_content[i] == '}':
+            def parsing_DefinitionBlock(inside_bracket):
+                spl = inside_bracket.split(',')
+                name = spl[1].replace('"', '').strip() + \
+                    '-' + spl[4].replace('"', '').strip()
+                return name
+
+            def parsing_OperationRegion(inside_bracket):
+                pass
+
             block_info = stack.pop()
-            content = dsdt_content[block_info[0], i+1]
+            content = dsdt_content[block_info[0]:i+1]
             block_type = block_info[1]
-            name = block_info[2]
+            if block_type in abandon_list:
+                continue
+            inside_bracket = block_info[2]
+            get_name = {
+                "DefinitionBlock": parsing_DefinitionBlock,
+                "Method": lambda arg: arg,
+                "Scope": lambda arg: arg,
+                "Device": lambda arg: arg,
+                "OperationRegion": parsing_OperationRegion,
+            }
+            name = get_name[block_type](inside_bracket)
+            # TODO: 加上路径解析
             block_info = {
                 'name': name,
                 'type': block_type,
                 'content': content,
             }
+            blocks.append(block_info)
+    return blocks
+
+
+def load_file():
+    with open('SSDT-BATC.dsl', 'r') as f:
+        content = f.read()
+        return content
+
+
+if __name__ == '__main__':
+    print(to_code_blocks(load_file()))
